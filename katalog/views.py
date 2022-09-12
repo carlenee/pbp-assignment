138425d2ead1e36ca061a011d2fc4b442d565c70
carlenee <carleneannabel@gmail.com>
2022-09-12 16:47:20 +0700

    mengimplementasikan fungsi views dan routing

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+data_katalog_item = CatalogItem.objects.all()
+context = {
+    'list_katalog': data_katalog_item,
+    'nama': 'Carlene Annabel',
+    'student_id': '2106752211'
+}
+
+def show_catalog(request):
+    return render(request, "katalog.html", context)
